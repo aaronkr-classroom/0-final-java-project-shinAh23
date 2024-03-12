4ae16d3b2c7e8ac1637d52e3b6972dd8bb37edbd
shinAh23 <162276879+shinAh23@users.noreply.github.com>
2024-03-12 14:49:20 +0900

    level 1 끝!~

---
+++ src/BookMarket.java
@@ -0,0 +1,11 @@
+
+public class BookMarket {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to shopping mall!");
+		System.out.println("welcome to market!");
+		
+	}
+
+}
